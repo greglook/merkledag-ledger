a86261fe8e6f3ba6e731e60a447eb5dec875fb81
Greg Look <greg@greg-look.net>
2015-12-28 22:44:46 -0800

    A few minor additions to print ns.

--- src/merkledag/data/finance/print.clj
+++ src/merkledag/data/finance/print.clj
@@ -34,7 +34,7 @@
 
 ;; ## Entry Rendering Methods
 
-(defn render-amount
+(defn render-quantity
   "Renders a financial quantity."
   [quantity]
   (case (:commodity quantity)
@@ -51,10 +51,12 @@
         unused (dissoc posting :account)]
     (str
       (if-let [amount (:amount posting)]
-        (format "    %-45s %20s" account-name (render-amount amount))
+        (format "    %-45s %20s" account-name (render-quantity amount))
         (format "    %s" account-name))
+      ; TODO: lot cost, lot date, price, balance
       (when-let [time (:time posting)]
         (str "\n        ; time: " time))
+      ; TODO: items
       (when-let [sources (seq (:sources posting))]
         (str/join (map #(str "\n        ; source: " (name (:source %)) "|" (:line %)) sources)))
       (when-let [unused (not-empty (dissoc posting :account :amount :time :sources))]
@@ -75,12 +77,8 @@
        ; meta
        ; comments
        (when-let [postings (seq (:postings tx))]
+         ; TODO: when there are only two postings and no special costs or prices, elide the negative posting amount?
          (str/join "\n" (map render-posting postings)))))
-
-
-(defmethod render-entry :default
-  [entry]
-  (str "; " (pr-str entry)))
 
 
 (defmethod render-entry :CommentBlock
@@ -91,3 +89,8 @@
 (defmethod render-entry :Transaction
   [[_ tx]]
   (render-transaction tx))
+
+
+(defmethod render-entry :default
+  [entry]
+  (str "; Unknown entry " (pr-str entry)))
