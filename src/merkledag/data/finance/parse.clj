3f0036db04f7f2e7a2a15d9b0a5b52fd1b7bb0af
Greg Look <greg@greg-look.net>
2016-01-29 01:05:00 -0800

    Streamline transaction integration.

--- src/merkledag/data/finance/parse.clj
+++ src/merkledag/data/finance/parse.clj
@@ -449,17 +449,17 @@
   (when-not *book-name*
     (throw (RuntimeException. "Must bind *book-name* to integrate transactions!")))
   (let [date (::date entry)
-        ledger-path [:books *book-name* :ledger
-                     (str (time/year date))
-                     (format "%02d" (time/month date))
-                     (format "%02d" (time/day date))]
-        ledger (or (get-in data ledger-path)
-                   {:data/type :finance/ledger
+        entry (dissoc entry ::date)]
+    (update-in data
+      [:books *book-name* :ledger
+       (str (time/year date))
+       (format "%02d" (time/month date))
+       (format "%02d" (time/day date))]
+      (fnil update {:data/type :finance/ledger
                     :time/date date
                     :finance.ledger/transactions []})
-        new-ledger (update ledger :finance.ledger/transactions
-                           #(->> (dissoc entry ::date) (conj %) (sort-by :time/at) (vec)))]
-    (assoc-in data ledger-path new-ledger)))
+      :finance.ledger/transactions
+      #(vec (sort-by :time/at (conj % entry))))))
 
 
 ; TODO: (transaction-seq ledger from to)
