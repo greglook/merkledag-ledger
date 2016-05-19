7d4cdc75e3cb1437f7b6bcafaa4dddb9a2688ae1
Greg Look <greg@greg-look.net>
2016-05-19 14:51:11 -0700

    Add commodity ns with duplicate price search.

---
+++ src/merkledag/data/finance/commodity.clj
@@ -0,0 +1,22 @@
+(ns merkledag.data.finance.commodity
+  "Functions dealing with financial commodities and prices."
+  (:require
+    [datascript.core :as d]))
+
+
+; Static checks:
+; - schema validates
+
+; Historical checks:
+; - check for duplicate prices?
+
+
+(defn find-duplicate-prices
+  [db]
+  (d/q '[:find ?p1 ?p2
+         :where [(< ?p1 ?p2)]
+                [?p1 :finance.price/commodity ?c]
+                [?p2 :finance.price/commodity ?c]
+                [?p1 :time/at ?time]
+                [?p2 :time/at ?time]]
+       db))
